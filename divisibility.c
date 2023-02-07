45081524ecd433609ecf2e5c662f99d09f08cff3
phayee17 <123065450+phayee17@users.noreply.github.com>
2023-02-07 23:03:59 +0300

    Add files via upload

---
+++ divisibility.c
@@ -0,0 +1,14 @@
+//divisibility test
+#include <stdio.h>
+int main()
+{
+  int a;
+  printf("Enter a value");
+  scanf("%d",&a);
+  if (a%5==0){
+    printf("a is divisible by 5");}
+  else {
+    printf("a is not divisible by 5");}
+  return 0;
+  }
+  
