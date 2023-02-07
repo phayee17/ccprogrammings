bfdf795b62ec4f5588eef8be99f9861a86027321
phayee17 <123065450+phayee17@users.noreply.github.com>
2023-02-07 23:04:58 +0300

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,14 @@
+//program to determine even and odd numbers
+#include <stdio.h>
+int main()
+{
+  int a;
+  printf("Enter a value");
+  scanf("%d",&a);
+  if(a%2==0){
+    printf("a is even");}
+  else{
+    printf("a is odd");}
+  return 0;
+  }
+  
